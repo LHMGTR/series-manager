10e4ee93a623065f5ca3a0ef47f31d418ad58885
ronnie <ronnie@ramalus-linux>
2013-04-23 21:03:01 -0300

    Testing new commit

---
+++ src/series/manager/SeriesManager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package series.manager;
+
+/**
+ *
+ * @author ronnie
+ */
+public class SeriesManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFORM mf = new MainFORM();
+        mf.setVisible(true);
+        //testint new commit
+        // TODO code application logic here
+    }
+}
